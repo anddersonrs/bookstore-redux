c86ebdbd8a2ec7f39bb11d3d91a906099c2d7d63
Anderson Rodrigues <anddersonrds@gmail.com>
2020-03-04 10:50:52 -0300

    add styles in home and cart pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,96 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './Home.styled';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
+          alt="books"
+        />
+        <strong>Hunter x Hunter</strong>
+        <span>R$ 21,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
