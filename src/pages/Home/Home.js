84694edebc76c379606c7daa5856917f89dd90b8
Anderson Rodrigues <anddersonrds@gmail.com>
2020-03-04 12:19:10 -0300

    get products with api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,96 +1,48 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../utils/format';
+import api from '../../services/api';
 
 import { ProductList } from './Home.styled';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://m.media-amazon.com/images/I/71ymvVR1QnL._AC_UL320_ML3_.jpg"
-          alt="books"
-        />
-        <strong>Hunter x Hunter</strong>
-        <span>R$ 21,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
