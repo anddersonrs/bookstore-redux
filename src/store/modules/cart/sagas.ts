98f34bfdbe92ca581a604ff648a102c29c0ba385
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-19 02:16:11 -0300

    WIP

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,23 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { addProductToCart } from './actions';
 
-const checkProductStock = () => {
+import { IState } from '../../index';
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
+
   console.log('Adiciounou ao carrinho');
-};
+}
 
 export default all([takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)]);
